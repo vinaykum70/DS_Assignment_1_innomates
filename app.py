51bed68a601c5d5fa9683ece557acda27ac261cd
vinaykum70 <122093835+vinaykum70@users.noreply.github.com>
2023-02-27 16:51:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+
+
+
+st.title(':red[Welcome !]')
+
+st.header('Innomates Data Science Internship 2023 !')
+
+st.header('This is my first streamlit assignment')
+
+st.snow()
+
+
+if(st.button('Click Me !')==True):
+    st.subheader('You clicked me :sunglasses:')
+    st.balloons()
+
+st.image('resources\images\AI.jpg')
+
+
+
